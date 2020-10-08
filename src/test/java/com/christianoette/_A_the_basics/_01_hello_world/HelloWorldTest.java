b3e5c1a28c8f667ead18a4fb05cf154fe48978a0
lakemaster <lakelord@jojobi.de>
2020-10-08 09:39:23 +0200

    first hello world test

--- src/test/java/com/christianoette/_A_the_basics/_01_hello_world/HelloWorldTest.java
+++ src/test/java/com/christianoette/_A_the_basics/_01_hello_world/HelloWorldTest.java
@@ -3,14 +3,59 @@ package com.christianoette._A_the_basics._01_hello_world;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest
+@SpringBootTest(classes = HelloWorldTest.TestConfig.class)
 class HelloWorldTest {
 
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
     @Test
-    @Disabled // TODO Remove disabled in your project if test won't start!
-    void test() {
-        // start here!
+    void test() throws Exception {
+        jobLauncherTestUtils.launchJob();
+    }
+
+    @Configuration
+    @EnableBatchProcessing
+    static class TestConfig {
+
+        @Autowired
+        private JobBuilderFactory jobBuilderFactory;
+
+        @Autowired
+        private StepBuilderFactory stepBuilderFactory;
+
+        @Bean
+        public Job helloWorldJob() {
+            Step step = stepBuilderFactory.get("step")
+                    .tasklet((stepContribution, chunkContext) -> {
+                        System.out.println("Hello World");
+                        return RepeatStatus.FINISHED;
+                    }).build();
+
+            return jobBuilderFactory.get("helloWorldJob")
+                    .start(step)
+                    .build();
+        }
+
+        @Bean
+        public JobLauncherTestUtils utils() {
+            return new JobLauncherTestUtils();
+        }
     }
 }
